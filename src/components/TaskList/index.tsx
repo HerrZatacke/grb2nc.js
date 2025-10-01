cf7f1635bdaf284a6d0ea8857ad173633c6cdb23
Andreas Hahn <zatacke@gmail.com>
2025-10-02 00:31:21 +0200

    Filelist styles

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,12 +1,17 @@
 'use client';
 
 import {useMainContext} from "@/components/MainContext";
+import './styles.scss';
 
 export default function TaskList() {
   const { tasks } = useMainContext();
   return (
-    <div>
-      <pre>{JSON.stringify(tasks.map(({ flip, type }) => ({ flip, type })), null, 2)}</pre>
+    <div className="task-list">
+      {tasks.map(({ fileName, type }) => (
+        <div className="task-list__entry">
+          { fileName }: { type }
+        </div>
+      ))}
     </div>
   );
 }
