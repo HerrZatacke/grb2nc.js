517f8d7a0cde867fbb2edd0fa9dc5a24a7e4c892
Andreas Hahn <zatacke@gmail.com>
2025-10-14 01:12:02 +0200

    linting and some typesafety

--- src/modules/sortTasks/index.ts
+++ src/modules/sortTasks/index.ts
@@ -1,9 +1,10 @@
 import { Task, TaskType } from '@/types/tasks';
 
 const taskValues: Record<TaskType, number> = {
-  [TaskType.EDGE_CUT]: 3,
-  [TaskType.DRILL]: 2,
   [TaskType.ISOLATION]: 1,
+  [TaskType.DRILL]: 2,
+  [TaskType.EDGE_CUT]: 3,
+  [TaskType.DRAWING]: 4,
 };
 
 export const sortTasks = (taskA: Task, taskB: Task): number => {
