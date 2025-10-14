1cff61ec567c35703975b3eca7f75eb2635b97d8
Andreas Hahn <zatacke@gmail.com>
2025-10-14 18:24:58 +0200

    Release Version 1.0.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import TaskList from '@/components/TaskList';
 import TaskParamsForm from '@/components/TaskParamsForm';
 
 export default function Home() {
+  const release = process.env.NEXT_PUBLIC_RELEASE_VERSION || '';
   return (
     <>
       <SVGPreview />
@@ -18,12 +19,13 @@ export default function Home() {
         <OperationForm />
         <TaskParamsForm />
       </div>
-      <footer  className="page-footer">
+      <footer className="page-footer">
         <a
+          className="page-footer__link"
           href="https://github.com/HerrZatacke/grb2nc.js"
           target="_blank"
         >
-          grb2nc.js on GitHub
+          {`grb2nc.js on GitHub (Version: ${release})`}
         </a>
       </footer>
     </>
